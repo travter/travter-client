a1413cab7a07859f9e0aedd3d20437f139664a9c
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-06-23 17:40:58 +0200

    popular people widget

--- lib/presentation/pages/home_page/widgets/popular_peoples_widget.dart
+++ lib/presentation/pages/home_page/widgets/popular_peoples_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/constants/constant_colors.dart';
 import '../../../core/constants/constant_dimensions.dart';
 import '../../../core/extensions.dart';
+import 'popular_person_card_widget.dart';
 import 'popular_section_text_widget.dart';
 
 class PopularPeoplesWidget extends StatelessWidget {
@@ -18,8 +20,30 @@ class PopularPeoplesWidget extends StatelessWidget {
         vertical: height * 0.05,
       ),
       child: Column(
-        children: const [
-          PopularSectionTextWidget(text: 'Popular People'),
+        children: [
+          const PopularSectionTextWidget(text: 'Popular People'),
+          Divider(
+            height: height * 0.05,
+            color: lightPrimaryColor,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              PopularPersonCardWidget(),
+              PopularPersonCardWidget(),
+            ],
+          ),
+          Divider(
+            height: height * 0.025,
+            color: lightPrimaryColor,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              PopularPersonCardWidget(),
+              PopularPersonCardWidget(),
+            ],
+          ),
         ],
       ),
     );
---
+++ lib/presentation/pages/home_page/widgets/popular_person_card_widget.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/constant_colors.dart';
+import '../../../core/extensions.dart';
+
+const _assetPath = 'assets/images';
+
+class PopularPersonCardWidget extends StatelessWidget {
+  const PopularPersonCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = context.dims.width;
+
+    return Container(
+      width: width * 0.425,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: primaryColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          _ImageWidget(),
+          _PlaceNameWidget(),
+          _ShortBioWidget(),
+          _JourneysMade(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ImageWidget extends StatelessWidget {
+  const _ImageWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final height = context.dims.height;
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: height * 0.015),
+      child: Center(
+        child: CircleAvatar(
+          radius: context.dims.width * 0.1,
+          backgroundImage: const AssetImage('$_assetPath/profile_picture.jpeg'),
+        ),
+      ),
+    );
+  }
+}
+
+class _PlaceNameWidget extends StatelessWidget {
+  const _PlaceNameWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = context.dims.width;
+    final height = context.dims.height;
+
+    return Padding(
+      padding: EdgeInsets.only(
+        left: width * 0.025,
+        top: height * 0.01,
+      ),
+      child: const Text(
+        'Wiktor Zając',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class _ShortBioWidget extends StatelessWidget {
+  const _ShortBioWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = context.dims.width;
+    final height = context.dims.height;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: width * 0.025,
+        vertical: height * 0.01,
+      ),
+      child: const Text(
+        'Big fan of nature and unknown places!',
+        style: TextStyle(
+          color: Colors.grey,
+          fontSize: 11,
+        ),
+      ),
+    );
+  }
+}
+
+class _JourneysMade extends StatelessWidget {
+  const _JourneysMade({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = context.dims.width;
+    final height = context.dims.height;
+
+    return Padding(
+      padding: EdgeInsets.only(
+        left: width * 0.025,
+        bottom: height * 0.01,
+      ),
+      child: Row(
+        children: [
+          const Icon(
+            Icons.directions_walk,
+            size: 15,
+            color: lightGreenColor,
+          ),
+          Text(
+            'Jorneys made: ',
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.85),
+              fontSize: 12,
+            ),
+          ),
+          const Text(
+            '13',
+            style: TextStyle(
+              color: lightGreenColor,
+              fontSize: 12,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
