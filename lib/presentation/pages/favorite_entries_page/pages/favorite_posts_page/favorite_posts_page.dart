6634534d14e2362986569f9a4dbd9824dec763dd
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-06-27 00:51:15 +0200

    finish mvp ui

--- lib/presentation/pages/favorite_entries_page/pages/favorite_posts_page/favorite_posts_page.dart
+++ lib/presentation/pages/favorite_entries_page/pages/favorite_posts_page/favorite_posts_page.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
 
 import '../../../../core/constants/constant_colors.dart';
+import '../../../../core/extensions.dart';
+import '../../../../core/widgets/bottom_navbar_widget.dart';
+import '../../../../core/widgets/travel_card.dart';
+import '../../load_navbar_children.dart';
 
 class FavoritePostsPage extends StatelessWidget {
   const FavoritePostsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
         backgroundColor: lightPrimaryColor,
-        body: Text('Favorite Posts'),
+        body: Column(
+          children: [
+             Container(
+                height: context.dims.height * 0.8,
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: context.dims.height * 0.05),
+                    child: Column(
+                      children: const [
+                        TravelCard(),
+                        TravelCard(),
+                        TravelCard(),
+                        TravelCard(),
+                      ],
+                ),
+                  ),
+              ),
+            ),
+            BottomNavbarWidget(children: loadChildren(context)),
+          ],
+        ),
       ),
     );
   }
