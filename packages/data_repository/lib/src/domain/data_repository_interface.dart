0f3ea8349ff8db813fffb6da72088dabf303efba
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-06-28 01:47:30 +0200

    data repository interface

--- packages/data_repository/lib/src/domain/data_repository_interface.dart
+++ packages/data_repository/lib/src/domain/data_repository_interface.dart
@@ -1,6 +1,34 @@
+import '../../data_repository.dart';
+import 'collaborative_journey/collaborative_journey.dart';
 import 'core/typedefs.dart';
 import 'expenses_tracker/expenses_tracker.dart';
+import 'journey/journey.dart';
+import 'request_failure.dart';
 
 abstract class DataRepositoryInterface {
   Future<RequestResult> createExpenseTracker(ExpensesTracker tracker);
+
+  Future<RequestFailure> removeExpenseTracker(String id);
+
+  Future<RequestResult> updateExpenseTracker(ExpensesTracker tracker);
+
+  Future<RequestResult> createExpense(Expense expense);
+
+  Future<RequestResult> removeExpense(String id);
+
+  Future<RequestResult> updateExpense(Expense expense);
+
+  Future<RequestResult> createJourney(Journey journey);
+
+  Future<RequestResult> removeJourney(String id);
+
+  Future<RequestResult> updateJourney(Journey journey);
+
+  Future<RequestResult> createCollaborativeJourney(
+      CollaborativeJourney collaborativeJourney);
+
+  Future<RequestResult> removeCollaborativeJourney(String id);
+
+  Future<RequestResult> updateCollaborativeJourney(
+      CollaborativeJourney collaborativeJourney);
 }
--- packages/data_repository/lib/src/infrastructure/data_repository.dart
+++ packages/data_repository/lib/src/infrastructure/data_repository.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 
+import '../domain/collaborative_journey/collaborative_journey.dart';
 import '../domain/core/firestore_exception.dart';
 import '../domain/core/typedefs.dart';
 import '../domain/data_repository_interface.dart';
+import '../domain/expenses_tracker/expense.dart';
 import '../domain/expenses_tracker/expenses_tracker.dart';
+import '../domain/journey/journey.dart';
 import '../domain/request_failure.dart';
 
 class DataRepository implements DataRepositoryInterface {
@@ -30,5 +33,71 @@ class DataRepository implements DataRepositoryInterface {
     }
 
     return right(unit);
+  }
+
+  @override
+  Future<RequestResult> createCollaborativeJourney(CollaborativeJourney collaborativeJourney) {
+    // TODO: implement createCollaborativeJourney
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestResult> createExpense(Expense expense) {
+    // TODO: implement createExpense
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestResult> createJourney(Journey journey) {
+    // TODO: implement createJourney
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestResult> removeCollaborativeJourney(String id) {
+    // TODO: implement removeCollaborativeJourney
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestResult> removeExpense(String id) {
+    // TODO: implement removeExpense
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestFailure> removeExpenseTracker(String id) {
+    // TODO: implement removeExpenseTracker
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestResult> removeJourney(String id) {
+    // TODO: implement removeJourney
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestResult> updateCollaborativeJourney(CollaborativeJourney collaborativeJourney) {
+    // TODO: implement updateCollaborativeJourney
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestResult> updateExpense(Expense expense) {
+    // TODO: implement updateExpense
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestResult> updateExpenseTracker(ExpensesTracker tracker) {
+    // TODO: implement updateExpenseTracker
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestResult> updateJourney(Journey journey) {
+    // TODO: implement updateJourney
+    throw UnimplementedError();
   }
 }
